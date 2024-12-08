e5829506048c25798e7f49f57ab919192ba22a56
Siddharth Rai <sidrai@github.com>
2024-12-08 11:56:06 +0000

    terraform script

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+for user_dir in users/*/; do
+  terraform -chdir=$user_dir init
+  terraform -chdir=$user_dir plan -var-file=$(basename $user_dir).tfvars
+  terraform -chdir=$user_dir apply -var-file=$(basename $user_dir).tfvars -auto-approve
+done
